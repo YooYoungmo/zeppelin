7ccf318f0344dab3b51918e502bc14a18a8f8a79
yooyoung-mo <gigamadness@gmail.com>
2017-05-15 09:54:19 +0900

    [https://bettercode.dooray.com/project/projects/(Tony)-No.16/21] Link Parameters Modal Angular Component 추가 및 설정 메뉴에 적용

---
+++ zeppelin-web/src/components/linkParameter/linkparameter.controller.js
@@ -0,0 +1,54 @@
+angular.module('zeppelinWebApp').controller('LinkParameterCtrl', LinkParameterCtrl);
+
+
+
+function LinkParameterCtrl ($scope) {
+  'ngInject'
+
+  $scope.data = {
+    linkColumn: null,
+    paragraph: null,
+    availableOptions: [
+      {id: '1', name: 'cust_no'},
+      {id: '2', name: '_col1'}
+    ],
+    linkParameterRows: [{
+      column : '',
+      inputName : ''
+    }],
+    addedLinks: []
+  };
+
+  $scope.addRow = function(index){
+    var emptyRow = { column : '', inputName : '' };
+    $scope.data.linkParameterRows.splice(index + 1, 0, emptyRow);
+  };
+
+  $scope.deleteRow = function(index) {
+    if(index > 0) {
+      $scope.data.linkParameterRows.splice(index, 1);
+    }
+  };
+
+  $scope.addLink = function() {
+    var rows = $scope.data.linkParameterRows;
+
+    var linkParameters = [];
+    for(var i = 0; i < rows.length; i++) {
+      var linkParameter = {};
+      linkParameter[rows[i].column] = rows[i].inputName;
+      linkParameters.push(linkParameter);
+    }
+
+    var addingLink = {
+      linkColumn: $scope.data.linkColumn,
+      targetParagraph: $scope.data.paragraph,
+      linkParameters:linkParameters
+    }
+    $scope.data.addedLinks.push(addingLink);
+  };
+
+  $scope.deleteLink = function(index) {
+    $scope.data.addedLinks.splice(index, 1);
+  };
+}
